5cc6cc6fb709592477b5f87761411590e9cce7ca
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-06-13 00:41:43 +0000

    task 3 API status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,30 @@
+/* API status
+Based on 1-hbnb.js
+Request http://0.0.0.0:5001/api/v1/status/
+*/
+$(document).ready(function () {
+  const amens = {};
+  $('input:checkbox').click(function () {
+    $(this).each(function () {
+      if (this.checked) {
+        amens[$(this).data('id')] = $(this).data('name');
+      } else {
+        delete amens[$(this).data('id')];
+      }
+    });
+    if (Object.values(amens).length > 0) {
+      $('.amenities h4').text(Object.values(amens).join(', '));
+    } else {
+      $('.amenities h4').html('&nbsp');
+    }
+  });
+});
+
+$.get('http://127.0.0.1:5001/api/v1/status/', function (data, status) {
+  console.log(data);
+  if (data.status === 'OK') {
+    $('#api_status').addClass('available');
+  } else {
+    $('#api_status').removeClass('available');
+  }
+});
